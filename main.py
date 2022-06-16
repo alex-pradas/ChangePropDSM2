7154e10f0b0bf3e34c55413bea263e43fac89f87
alex-pradas <alex@pradas.eu>
2022-06-16 17:13:25 +0200

    Likelihood and Impact added
    Arindam had the issue where he needed to add an extra row to the csv files with an extra value. before it was just substituting an X with a value, but now it is adding a second row with all empty commas except the likehood values which will be taken from a second column.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from pathlib import Path
+import pandas as pd
+
 # The name of the folder where all inputs are stored
 IN_DIR = "data"
 # Folder name where all new data will be written to
 OUT_DIR = "output"
 # Name of the file where change propagation values are stored
 CP_VALUES = "Values.xlsx"
-
-import pandas as pd
-from pathlib import Path
+# Name of the Excel sheet_name/tab in CP_VALUES excel from where the values will be extracted from
+SHEET = "Sheet1"
 
 
 def merge_1_file(filename):
@@ -15,38 +17,52 @@ def merge_1_file(filename):
     dsm = dsm.set_index("HIERARCHY ID", drop=False)
     element = dsm[["ELEMENT NAME", "HIERARCHY ID"]].set_index("HIERARCHY ID").to_dict()["ELEMENT NAME"]
 
-    cp = pd.read_excel(f"{IN_DIR}/{CP_VALUES}")
+    cp = pd.read_excel(f"{IN_DIR}/{CP_VALUES}", sheet_name=SHEET)
 
     def find_cp_value(a, b, cp):
         """Looks for a couple of element text description and returns the change propagation value
         Inputs:
             a: Name of the first element to look
             b: Name of the second element to look
-            cp: pandas DataFrame containing the names of the first two columns and the change propagation value in the third
+            cp: pandas DataFrame containing the names of the first two columns and
+                the change propagation value in the third
 
-        This funcion assumes that the dataframe is named "Element 1", "Element 2" and "Value"
+        This function assumes that the dataframe is named "Element 1", "Element 2" and "Value"
 
         """
         possible = cp[(cp["Element 1"] == a) & (cp["Element 2"] == b)]
         if len(possible) == 1:
-            return str(possible["Value"].tolist()[0])
+            return str(possible["Likelihood"].tolist()[0]), str(possible["Impact"].tolist()[0])
         else:
             possible = cp[(cp["Element 1"] == b) & (cp["Element 2"] == a)]
             if len(possible) == 1:
-                return str(possible["Value"].tolist()[0])
+                return str(possible["Likelihood"].tolist()[0]), str(possible["Impact"].tolist()[0])
             else:
-                raise ValueError(f"Could not find on {CP_VALUES} a Change Propagation value for the combination: \n"
+                raise ValueError(f"Could not find on {CP_VALUES} sheet {SHEET} a Change Propagation value for the combination: \n"
                                  f"- '{a}' \n"
                                  f"- '{b}'\n"
                                  f"The combination was found on {filename.name}")
-
+    dsm_likelihood = dsm.copy()
+    list_elements = dsm["HIERARCHY ID"].to_list()
     for row in dsm["HIERARCHY ID"].to_list():
         for col in dsm["HIERARCHY ID"].to_list():
             if isinstance(dsm.at[row, col], str):
-                dsm.loc[row, [col]] = find_cp_value(element[row], element[col], cp)
+                dsm.loc[row, [col]] = find_cp_value(element[row], element[col], cp)[0]
+                dsm_likelihood.loc[row, [col]] = find_cp_value(element[row], element[col], cp)[1]
 
+    # adding +0.0001 to index in likehood to add the two dsm's df together
+    dsm.reset_index(inplace=True, drop=True)
+    dsm_likelihood.reset_index(inplace=True, drop=True)
+    dsm_likelihood.reset_index(inplace=True)
+    dsm_likelihood.set_index(dsm_likelihood["index"] + 0.1, inplace=True)
+    dsm_likelihood.drop(columns=["index", "CLUSTER LEVEL 0","ELEMENT NAME","HIERARCHY ID"], inplace=True)
+
+    dsm_combined = pd.concat([dsm, dsm_likelihood]).sort_index()
+
+    #old stuff
     out_filename = Path(OUT_DIR) / filename.name
-    dsm.to_csv(out_filename, index=False)
+
+    dsm_combined.to_csv(out_filename, index=False)
 
 
 def rm_tree(pth):
@@ -61,7 +77,7 @@ def rm_tree(pth):
 
 def main():
     p_out = Path(OUT_DIR)
-    # deleting and recreate output folder if not exist
+
     print(f"deleting and recreating {p_out}")
     rm_tree(p_out)
     p_out.mkdir(parents=True, exist_ok=True)
@@ -70,8 +86,6 @@ def main():
         print(f"updating {file}")
         merge_1_file(file)
     print("\n*** Done! ***\n")
-
-
 
 
 if __name__ == "__main__":
